91de574c3ffc32e0848db3facd016462346eea02
Marek Newton <marek.s.newton@gmail.com>
2016-07-04 11:27:10 -0600

    Add stuff

--- main.js
+++ main.js
@@ -12,6 +12,20 @@ shipImage.src = "ship.png"
 var spaceRockImg = new Image()
 spaceRockImg.src = "spaceRock.png"
 
+var explodingImg = new Image()
+explodingImg.src = "explodingPic.png"
+//game varibles
+var gameGoing = true
+var score = 0
+//game functions
+function drawScore() {
+	ctx.beginPath()
+	ctx.fillStyle = "white"
+	ctx.fill()
+	ctx.font = "30px Arial"
+	ctx.fillText(score,10,30)
+	ctx.closePath()
+}
 document.addEventListener("keydown", function(e) {
 	if(e.keyCode == 65) {
 		ship.movingRight = true
@@ -22,6 +36,15 @@ document.addEventListener("keydown", function(e) {
 	if(e.keyCode == 32) {
 		ship.bullet.shooting = true
 		laserSound.play();
+	}
+	if(e.keyCode == 82) {
+		if(gameGoing == false) {
+			console.log("panda")
+			score = 0
+			ship.x = 190
+			ship.alive = true
+			setInterval(draw,10)
+		}
 	}
 }, false);
 
@@ -37,8 +60,14 @@ document.addEventListener("keyup", function(e) {
 var ship = {
 	x:190,
 	y:550,
+	alive: true,
 	draw: function() {
-		ctx.drawImage(shipImage, this.x,this.y)
+		if(this.alive == false) {
+			ctx.drawImage(explodingImg, this.x,this.y)
+		}
+		else {
+			ctx.drawImage(shipImage, this.x,this.y)
+		}
 	},
 	movingRight: false,
 	movingLeft: false,
@@ -83,47 +112,235 @@ var ship = {
 		if(this.bullet.y < 0) {
 			this.bullet.shooting = false
 			this.bullet.y = 554
+			this.bullet.x1 = this.x + 9
+			this.bullet.x2 = this.x + 30
 		}
 	}
 }
-var spaceRock = {
-	x: 100,
-	y: 100,
-	alive: true;
-	draw: function() {
-		/*for(i = 0;i < 400;i++) {
-			if(i % 100 == 0) {
-				spaceRock.y = i
-			}*/
-			if(this.alive == true) {
-				ctx.drawImage(spaceRockImg ,this.x,this.y)
-			}
-			else {
-				ctx.drawImage()
-			}
-			//console.log(this.y)
-		//}
-	},
-	move: function() {
-		this.y += 1
-		console.log(this.y)
-		if(this.y >= 600) {
-			this.y = 100
-		}
-	},
-	getShot: function() {
-		if(ship.bullet.y < this.y) {
 
-		}
+var spaceRockNumber = 0
+function spaceRock(x,y,alive,speed) {
+	this.x = x
+	this.y = y
+	this.alive = alive
+	this.speed = speed
+	spaceRockNumber += 1
+}
+var spaceRock1 = new spaceRock(0,0,true,1)
+var spaceRock2 = new spaceRock(0,-100,true,1)
+var spaceRock3 = new spaceRock(0,-200,true,1) 
+var spaceRock4 = new spaceRock(0,-300,true,1)
+var spaceRock5 = new spaceRock(0,-400,true,1)  
+function drawSpaceRock() {
+	if(spaceRock1.alive == false) {
+		ctx.drawImage(explodingImg,spaceRock1.x,spaceRock1.y)
 	}
+	else {
+		ctx.drawImage(spaceRockImg,spaceRock1.x,spaceRock1.y)
+	}
+	if(spaceRock2.alive == false) {
+		ctx.drawImage(explodingImg,spaceRock2.x,spaceRock2.y)
+	}
+	else {
+		ctx.drawImage(spaceRockImg,spaceRock2.x,spaceRock2.y)
+	}
+	if(spaceRock3.alive == false) {
+		ctx.drawImage(explodingImg,spaceRock3.x,spaceRock3.y)
+	}
+	else {
+		ctx.drawImage(spaceRockImg,spaceRock3.x,spaceRock3.y)
+	}
+	if(spaceRock3.alive == false) {
+		ctx.drawImage(explodingImg,spaceRock4.x,spaceRock4.y)
+	}
+	else {
+		ctx.drawImage(spaceRockImg,spaceRock4.x,spaceRock4.y)
+	}
+	if(spaceRock3.alive == false) {
+		ctx.drawImage(explodingImg,spaceRock5.x,spaceRock5.y)
+	}
+	else {
+		ctx.drawImage(spaceRockImg,spaceRock5.x,spaceRock5.y)
+	}
+}
+
+var timer1 = 0
+var timer2 = 0
+var timer3 = 0
+var timer4 = 0
+var timer5 = 0
+
+var timer1StopingPoint = Math.floor(Math.random() * 400)
+var timer2StopingPoint = Math.floor(Math.random() * 400)
+var timer3StopingPoint = Math.floor(Math.random() * 400)
+var timer4StopingPoint = Math.floor(Math.random() * 400)
+var timer5StopingPoint = Math.floor(Math.random() * 400)
+
+function moveSpaceRocks() {
+	spaceRock1.y += spaceRock1.speed
+	spaceRock2.y += spaceRock2.speed
+	spaceRock3.y += spaceRock3.speed
+	spaceRock4.y += spaceRock4.speed
+	spaceRock5.y += spaceRock5.speed
+	if(spaceRock1.y > canvas.height) {
+		spaceRock1.alive = true
+		spaceRock1.y = 0
+		timer1StopingPoint = Math.floor(Math.random() * 400)
+		
+	}
+	if(spaceRock2.y > canvas.height) {
+		spaceRock2.alive = true
+		spaceRock2.y = 0
+		timer2StopingPoint = Math.floor(Math.random() * 400)
+		
+	}
+	if(spaceRock3.y > canvas.height) {
+		spaceRock3.alive = true
+		spaceRock3.y = 0
+		timer3StopingPoint = Math.floor(Math.random() * 400)
+		ship.bullet.resetBullet()
+	}
+	if(spaceRock4.y > canvas.height) {
+		spaceRock4.alive = true
+		spaceRock4.y = 0
+		timer4StopingPoint = Math.floor(Math.random() * 400)
+		
+	}
+	if(spaceRock5.y > canvas.height) {
+		spaceRock5.alive = true
+		spaceRock5.y = 0
+		timer5StopingPoint = Math.floor(Math.random() * 400)
+		
+	}
+
+	/*Math.floor(Math.random() * 10)*/
+
+	
+	timer1 += 1
+	timer2 += 1 
+	timer3 += 1
+	timer4 += 1
+	timer5 += 1
+	if(timer1 == timer1StopingPoint) {
+		timer1 = 0
+	}
+	if(timer1 < timer1StopingPoint / 2) {
+		spaceRock1.x += 1
+	}
+	if(timer1 > timer1StopingPoint / 2) {
+		spaceRock1.x -= 1
+	}
+	
+
+	if(timer2 >= timer2StopingPoint) {
+		timer2 = 0
+	}
+	if(timer2 < timer2StopingPoint / 2) {
+		spaceRock2.x += 1
+	}
+	if(timer2 > timer2StopingPoint / 2) {
+		spaceRock2.x -= 1
+	}
+	
+
+	if(timer3 >= timer3StopingPoint) {
+		timer3 = 0
+	}
+	if(timer3 < timer3StopingPoint / 2) {
+		spaceRock3.x += 1
+	}
+	if(timer3 > timer3StopingPoint / 2) {
+		spaceRock3.x -= 1
+	}
+	
+
+	if(timer4 >= timer4StopingPoint) {
+		timer4 = 0
+	}
+	if(timer4 < timer4StopingPoint / 2) {
+		spaceRock4.x += 1
+	}
+	if(timer4 > timer4StopingPoint / 2) {
+		spaceRock4.x -= 1
+	}
+
+ 	if(timer5 >= timer5StopingPoint) {
+		time5 = 0
+	}
+ 	if(timer5 < timer5StopingPoint / 2) {
+		spaceRock5.x += 1
+	}
+	if(timer5 > timer5StopingPoint / 2) {
+		spaceRock5.x -= 1
+	}
+	
+	console.log(timer1)
+	console.log(timer2)
+	console.log(timer3)
+	console.log(timer4)
+	console.log(timer5)	
+}
+function spaceRocksGetShot() {
+	console.log(spaceRock1.x )
+	if(ship.bullet.y < spaceRock1.y && spaceRock1.y < ship.y 
+		&& ship.bullet.x2 > spaceRock1.x && spaceRock1.x + 50 > ship.bullet.x2) {
+		explodingSound.play()
+		spaceRock1.alive = false 
+		score += 1 
+	}
+	if(ship.bullet.y < spaceRock2.y && spaceRock2.y < ship.y && 
+		ship.bullet.x2 > spaceRock2.y && spaceRock2.x + 50 > ship.bullet.x2) {
+		explodingSound.play()
+		spaceRock2.alive = false 
+		score += 1 
+	}
+	if(ship.bullet.y < spaceRock3.y && spaceRock3.y < ship.y && 
+		ship.bullet.x2 > spaceRock3.y && ship.bullet.x2 < spaceRock3.x + 50) {
+		explodingSound.play()
+		spaceRock3.alive = false 
+		score += 1 
+	}
+	if(ship.bullet.y < spaceRock4.y && spaceRock4.y < ship.y &&
+		ship.bullet.x2 > spaceRock4.y && spaceRock4.x + 50 > spaceRock4.x) {
+		explodingSound.play()
+		spaceRock4.alive = false 
+		score += 1 
+	}
+	if(ship.bullet.y < spaceRock5.y && spaceRock5.y < ship.y &&
+		ship.bullet.x2 > spaceRock5.y && spaceRock5.x + 50 > ship.bullet.x2) {
+		explodingSound.play()
+		spaceRock5.alive = false 
+		score += 1 
+	}
+}
+function spaceRockKillsPlayer() {
+	if(spaceRock1.y > ship.y && spaceRock1.alive == true) {
+		if(ship.alive == true) {
+			explodingSound.play()
+		}
+		ship.alive = false
+		clearInterval(loop)
+		gameGoing = false
+		restartGameMenu()
+	} 
+}
+function restartGameMenu() {
+	ctx.beginPath()
+	ctx.fillStyle = "white"
+	ctx.fill()
+	ctx.fillText("Game Over Your Score Is " + score,8,100)
+	ctx.closePath()
 }
 function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	sss;
+	drawScore()
 	ship.draw()
 	ship.move()
 	ship.bullet.draw()
 	ship.resetBullet()
-	spaceRock.draw()
-	spaceRock.move()
+	drawSpaceRock()
+	moveSpaceRocks()
+	spaceRocksGetShot()
+	//spaceRockKillsPlayer()
 }
-setInterval(draw,10)
+var loop = setInterval(draw,10)
